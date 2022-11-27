63e8c2f94abedce8fa09fb2d5681c61450467044
Arkarhein <arkarhein.arkarsh@gmial.com>
2022-11-27 11:32:12 +0630

    built a random generator function in the JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
- console.log("Hello WQorld");
+function getComputerChoice() {
+         
+    let choice = Math.floor(Math.random()* 12 );
+    
+    if ( choice <= 4) return "Rock";
+    if ( choice <= 8) return "Paper";
+    if ( choice <= 12) return "Sicssor";
 
- alert(" Dummbies ")
+}
