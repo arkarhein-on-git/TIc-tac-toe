5035ac275f5f1447ca3a74fff34e1cd3449087b3
Arkarhein <arkarhein.arkarsh@gmial.com>
2022-12-13 12:35:20 +0630

    made some changes

--- js/app.js
+++ js/app.js
@@ -1,58 +1,64 @@
-function getComputerChoice() {
-         
-    let choice = ['rock','paper','scissor']
 
-    return choice[Math.floor(Math.random()* choice.length)]
+
+function getComputerChoice() {
+
+   let choice = ['rock','paper','scissor'];
+     return choice[Math.floor(Math.random()* choice.length)]
  }
-// function getPlayerChoice(){
-//       const player = prompt(`rock paper or siccsor?`);
-//       return player.toLowerCase();
-// }
 
 let playerScore = 0;
 let computerScore = 0;
 
 
-function playRound(playerSelection, computerSelection){
-          console.log(playerScore);
-          console.log(computerSelection);
-          
-    
-    if (playerSelection === computerSelection) return "Draw, play agin.";
 
-    if ( playerSelection === "rock" && computerSelection === "scissor" || 
+
+function playRound(playerSelection, computerSelection){
+
+
+   console.log( `Player Selection: ${playerSelection} , Computer Selection: ${computerSelection}, PlayerScore: ${playerScore}, ComputerScore: ${computerScore}`);
+
+
+    
+    if (playerSelection === computerSelection) {
+
+       return 'Draw'
+    } else if ( playerSelection === "rock" && computerSelection === "scissor" || 
          playerSelection === "scissor"&& computerSelection === "paper "||
          playerSelection === "paper" && computerSelection === "rock") {
             
-             playerScore = playerScore + 1;
+             playerScore++;
             return "You Won! You have defeated the Computer.";
             
 
-         } else if ( computerSelection === "rock" && playerSelec === "scissor" || 
-         computerSelection === "scissor"&& playerSelection === "paper"||
-         computerSelection === "paper" && playerSelection === "rock") {
-            computerScore = computerScore + 1;
-            return "Computer Won! You have been defeated."
+         } else {
+            computerScore++;
+            return "Computer Won! You have been defeated.";
             
          }
         
          
 }
 
-const playerSelection = 'rock';
-
 
 function playGame() {
         
        for ( let i=0 ; i < 5 ; i++) {
+
+            let playerChoice = prompt(`rock paper or scissor`);
+            console.log(`playerChoice : ${playerChoice}`)
+            console.log(typeof playerChoice);
+            const playerSelection = playerChoice.toLowerCase();
             const computerSelection = getComputerChoice();
-            console.log(playRound(playerSelection, computerSelection))
-       } 
-    
-     if (playerScore > computerScore) return "You defeated the Computer" ;
-     if (playerScore < computerScore) return "You lost ";
-            
+            playRound(playerSelection, computerSelection);
+        } 
+         if ( playerScore > computerScore ) {
+            return `Congratulations for the winning.`;
+         } else if ( computerScore > playerScore) {
+            return ` Sorry for your loss.`;
+         } else {
+            `Oh god, it was a draw`;
+         }
 } 
 
 
-playGame();
+ console.log (playGame());
