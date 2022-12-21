7a85fb3be3c259697fb0bdbf8a5b9f9500fbbbf9
Arkarhein <arkarhein.arkarsh@gmial.com>
2022-12-21 21:07:23 +0630

    added new featuers in the HTML and JS files

--- js/app.js
+++ js/app.js
@@ -1,64 +1,114 @@
 
 
+
+//Adding Dom for the buttons
+
+
+
+
+
+const newDiv = document.querySelector(".newdiv");
+
+const buttons = document.querySelectorAll('.player button');
+
+ buttons.forEach( function(button){
+   button.addEventListener('click', function(e) { 
+      const buttonName = e.target.textContent ;
+
+      const playerSelection = buttonName.toLowerCase() ;
+       
+
+      const computerSelection = getComputerChoice(); 
+
+
+     playRound(playerSelection, computerSelection) ;
+
+     whoWins(playerScore, computerScore);
+
+
+   });
+}); 
+
+function whoWins(playerScore , computerScore){
+       if (playerScore === 5) {
+         const h2 = document.createElement('h2');
+         h2.innerText = `You have beaten the Computer `;
+         newDiv.append(h2)
+       } else if (computerScore === 5) {
+         const h2 = document.createElement('h2');
+         h2.innerText = `You have been beaten by the Computer `;
+         newDiv.append(h2)
+       }
+
+}
+
+
 function getComputerChoice() {
 
-   let choice = ['rock','paper','scissor'];
+     let choice = ['rock','paper','scissor'];
      return choice[Math.floor(Math.random()* choice.length)]
  }
 
-let playerScore = 0;
-let computerScore = 0;
 
+
+
+
+
+ let playerScore = 0;
+ let computerScore = 0;
 
 
 
 function playRound(playerSelection, computerSelection){
 
+      if( playerSelection === computerSelection){
+         const p = document.createElement('p');
+         p.innerText = "Draw";
+         newDiv.appendChild(p);
 
-   console.log( `Player Selection: ${playerSelection} , Computer Selection: ${computerSelection}, PlayerScore: ${playerScore}, ComputerScore: ${computerScore}`);
+      } else if (playerSelection === "rock" &&
+      computerSelection === "scissor" ||
+      playerSelection === "scissor" &&
+      computerSelection === "paper" ||
+      playerSelection === "paper" &&
+      computerSelection === "rock")
+      {   
+         playerScore++ 
+         const p = document.createElement('p');
+         p.innerText = "You Won";
+         newDiv.appendChild(p);
 
+      } else {
+          computerScore++
+          const p = document.createElement('p');
+         p.innerText = "Computer Won!";
+         newDiv.appendChild(p);
 
-    
-    if (playerSelection === computerSelection) {
-
-       return 'Draw'
-    } else if ( playerSelection === "rock" && computerSelection === "scissor" || 
-         playerSelection === "scissor"&& computerSelection === "paper "||
-         playerSelection === "paper" && computerSelection === "rock") {
-            
-             playerScore++;
-            return "You Won! You have defeated the Computer.";
-            
-
-         } else {
-            computerScore++;
-            return "Computer Won! You have been defeated.";
-            
-         }
+      }
+      
+   
+   };
         
          
-}
+// }
+//function for playin 5 times;
 
-
-function playGame() {
+// function playGame() {
         
-       for ( let i=0 ; i < 5 ; i++) {
+//        for ( let i=0 ; i < 5 ; i++) {
 
-            let playerChoice = prompt(`rock paper or scissor`);
-            console.log(`playerChoice : ${playerChoice}`)
-            console.log(typeof playerChoice);
-            const playerSelection = playerChoice.toLowerCase();
-            const computerSelection = getComputerChoice();
-            playRound(playerSelection, computerSelection);
-        } 
-         if ( playerScore > computerScore ) {
-            return `Congratulations for the winning.`;
-         } else if ( computerScore > playerScore) {
-            return ` Sorry for your loss.`;
-         } else {
-            `Oh god, it was a draw`;
-         }
-} 
-
-
- console.log (playGame());
+//             let playerChoice = prompt(`rock paper or scissor`);
+//             console.log(`playerChoice : ${playerChoice}`)
+//             console.log(typeof playerChoice);
+//             const playerSelection = playerChoice.toLowerCase();
+//             const computerSelection = getComputerChoice();
+//             playRound(playerSelection, computerSelection);
+//         } 
+//          if ( playerScore > computerScore ) {
+//             return `Congratulations for the winning.`;
+//          } else if ( computerScore > playerScore) {
+//             return ` Sorry for your loss.`;
+//          } else {
+//             `Oh god, it was a draw`;
+//          }
+// } 
